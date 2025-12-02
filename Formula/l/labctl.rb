103428217723afb05195c8eba3c1fc89bfce1b07
raashed <mohammedraashed10@gmail.com>
2025-12-02 17:35:30 +0530

    labctl 0.1.50 (new formula)

--- Formula/l/labctl.rb
+++ Formula/l/labctl.rb
@@ -1,32 +1,14 @@
 class Labctl < Formula
   desc "CLI tool for interacting with iximiuz labs and playgrounds"
   homepage "https://github.com/iximiuz/labctl"
+  url "https://github.com/iximiuz/labctl/archive/refs/tags/v0.1.50.tar.gz"
+  sha256 "38373d00cc5fb0bec1f9332dcf7c21e320441be294b02f630ff1dff9d04c9d08"
   license "MIT"
 
-  version "0.1.50"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/iximiuz/labctl/releases/download/v0.1.50/labctl_darwin_arm64.tar.gz"
-      sha256 "6d4209f50f7cc1ae33bc2d492ffb5d2dd9083475dfc7d787c45e08acc2ca5ad1"
-    else
-      url "https://github.com/iximiuz/labctl/releases/download/v0.1.50/labctl_darwin_amd64.tar.gz"
-      sha256 "274ea09f6d93a5d0bf35b7767b842d6bbc9c5199ef9fd1dd324ba3c7fa0e1601"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/iximiuz/labctl/releases/download/v0.1.50/labctl_linux_arm64.tar.gz"
-      sha256 "df136bb19c164e6828fce045114708ee5170e053c756b5a410e1c6c6deeb74da"
-    else
-      url "https://github.com/iximiuz/labctl/releases/download/v0.1.50/labctl_linux_amd64.tar.gz"
-      sha256 "34f4f9c3643e37f2fabf26b01620d7f0b7bed34481ad9e1869a017478b356f4b"
-    end
-  end
+  depends_on "go" => :build
 
   def install
-    bin.install "labctl"
+    system "go", "build", *std_go_args
   end
 
   test do
