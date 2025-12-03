07d715cd4a7e20744ac0a4285cec29f31c111fef
raashed <mohammedraashed10@gmail.com>
2025-12-03 10:23:07 +0530

    labctl 0.1.50 (new formula)

--- Formula/l/labctl.rb
+++ Formula/l/labctl.rb
@@ -3,15 +3,16 @@ class Labctl < Formula
   homepage "https://github.com/iximiuz/labctl"
   url "https://github.com/iximiuz/labctl/archive/refs/tags/v0.1.50.tar.gz"
   sha256 "38373d00cc5fb0bec1f9332dcf7c21e320441be294b02f630ff1dff9d04c9d08"
-  license "MIT"
+  license "Apache-2.0"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
   end
 
   test do
-    assert_match "labctl", shell_output("#{bin}/labctl --help")
+    assert_match version.to_s, shell_output("#{bin}/labctl version")
   end
 end
